d06778cba07c881c9e12479c1bfeb16c7cbbc9e7
nikolamanushev48 <nikolamanushev48@gmail.com>
2021-03-08 22:43:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/first_project/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.first_project
+
+//import android.R
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.nikola)
+    }
+}
+
