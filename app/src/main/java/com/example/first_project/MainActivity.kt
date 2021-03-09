55a11b5aea0f326f6ad10fa452ea29c554c36a7c
lucho03 <l.valentinov.ivanov@gmail.com>
2021-03-09 17:29:45 +0200

    Button!

--- app/src/main/java/com/example/first_project/MainActivity.kt
+++ app/src/main/java/com/example/first_project/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.first_project
 
 //import android.R
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +11,11 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.button)
+        val button: Button = findViewById(R.id.button2)
+        button.setOnClickListener(){
+            setContentView(R.layout.activity_main)
+        }
     }
 }
 
