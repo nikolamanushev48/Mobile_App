cdc8eb3bd7966ed201a81b8a44269d91a38adc27
lucho03 <l.valentinov.ivanov@gmail.com>
2021-04-21 09:52:38 +0300

    White pawns

--- app/src/main/java/com/example/first_project/MainActivity.kt
+++ app/src/main/java/com/example/first_project/MainActivity.kt
@@ -15,53 +15,52 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener(){
             setContentView(R.layout.activity_main)
 
-
             black_pawn_1.setOnClickListener(){
                 black_pawn_1.animate().apply {
                     duration = 1000;
-                    translationY(-225f)
+                    translationY(225f)
                 }.start()
             }
             black_pawn_2.setOnClickListener(){
                 black_pawn_2.animate().apply {
                     duration = 1000;
-                    translationY(-225f)
+                    translationY(225f)
                 }.start()
             }
             black_pawn_3.setOnClickListener(){
                 black_pawn_3.animate().apply {
                     duration = 1000;
-                    translationY(-225f)
+                    translationY(225f)
                 }.start()
             }
             black_pawn_4.setOnClickListener(){
                 black_pawn_4.animate().apply {
                     duration = 1000;
-                    translationY(-225f)
+                    translationY(225f)
                 }.start()
             }
             black_pawn_5.setOnClickListener(){
                 black_pawn_5.animate().apply {
                     duration = 1000;
-                    translationY(-225f)
+                    translationY(225f)
                 }.start()
             }
             black_pawn_6.setOnClickListener(){
                 black_pawn_6.animate().apply {
                     duration = 1000;
-                    translationY(-225f)
+                    translationY(225f)
                 }.start()
             }
             black_pawn_7.setOnClickListener(){
                 black_pawn_7.animate().apply {
                     duration = 1000;
-                    translationY(-225f)
+                    translationY(225f)
                 }.start()
             }
             black_pawn_8.setOnClickListener(){
                 black_pawn_8.animate().apply {
                     duration = 1000;
-                    translationY(-225f)
+                    translationY(225f)
                 }.start()
             }
 
