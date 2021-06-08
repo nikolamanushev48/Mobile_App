3384465bfef7ad1584f58fad9d047bbcdb648d3a
lucho03 <l.valentinov.ivanov@gmail.com>
2021-06-08 23:47:15 +0300

    Try to run main with instance of other classes!

--- app/src/main/java/com/example/first_project/MainActivity.kt
+++ app/src/main/java/com/example/first_project/MainActivity.kt
@@ -1,13 +1,8 @@
 package com.example.first_project
 
-//import android.R
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-
-
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,113 +10,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.button)
         val button: Button = findViewById(R.id.button2)
         button.setOnClickListener(){
-            setContentView(R.layout.activity_main2)
-
-            black_pawn_1.setOnClickListener(){
-                black_pawn_1.animate().apply {
-                    duration = 1000;
-                    translationY(-125f)
-                }.start()
-            }
-            black_pawn_2.setOnClickListener(){
-                black_pawn_2.animate().apply {
-                    duration = 1000;
-                    translationY(-125f)
-                }.start()
-            }
-            black_pawn_3.setOnClickListener(){
-                black_pawn_3.animate().apply {
-                    duration = 1000;
-                    translationY(-125f)
-                }.start()
-            }
-            black_pawn_4.setOnClickListener(){
-                black_pawn_4.animate().apply {
-                    duration = 1000;
-                    translationY(-125f)
-                }.start()
-            }
-            black_pawn_5.setOnClickListener(){
-                black_pawn_5.animate().apply {
-                    duration = 1000;
-                    translationY(-125f)
-                }.start()
-            }
-            black_pawn_6.setOnClickListener(){
-                black_pawn_6.animate().apply {
-                    duration = 1000;
-                    translationY(-125f)
-                }.start()
-            }
-            black_pawn_7.setOnClickListener(){
-                black_pawn_7.animate().apply {
-                    duration = 1000;
-                    translationY(-125f)
-                }.start()
-            }
-            black_pawn_8.setOnClickListener(){
-                black_pawn_8.animate().apply {
-                    duration = 1000;
-                    translationY(-125f)
-                }.start()
-            }
-
-            white_pawn_1.setOnClickListener(){
-                white_pawn_1.animate().apply {
-                    duration = 1000;
-                    translationY(125f)
-                }.start()
-            }
-
-            white_pawn_2.setOnClickListener(){
-                white_pawn_2.animate().apply {
-                    duration = 1000;
-                    translationY(125f)
-                }.start()
-            }
-
-            white_pawn_3.setOnClickListener(){
-                white_pawn_3.animate().apply {
-                    duration = 1000;
-                    translationY(125f)
-                }.start()
-            }
-
-            white_pawn_4.setOnClickListener(){
-                white_pawn_4.animate().apply {
-                    duration = 1000;
-                    translationY(125f)
-                }.start()
-            }
-
-            white_pawn_5.setOnClickListener(){
-                white_pawn_5.animate().apply {
-                    duration = 1000;
-                    translationY(125f)
-                }.start()
-            }
-
-            white_pawn_6.setOnClickListener(){
-                white_pawn_6.animate().apply {
-                    duration = 1000;
-                    translationY(125f)
-                }.start()
-            }
-
-            white_pawn_7.setOnClickListener(){
-                white_pawn_7.animate().apply {
-                    duration = 1000;
-                    translationY(125f)
-                }.start()
-            }
-
-            white_pawn_8.setOnClickListener(){
-                white_pawn_8.animate().apply {
-                    duration = 1000;
-                    translationY(125f)
-                }.start()
-            }
+            val player: BlackPawns = BlackPawns()
+            player.setContentView(R.layout.activity_main2)
+            player.move()
         }
+
+        /*
+        var game: Int = 1// player = server.set_game()
+        if(game == 1) {
+            val player1: BlackPawns = BlackPawns(1)
+            player1.move()
+        }
+        if(game == 2) {
+            val player2: BlackPawns = BlackPawns()
+            player2.move()
+        }
+
+         */
     }
 }
 
