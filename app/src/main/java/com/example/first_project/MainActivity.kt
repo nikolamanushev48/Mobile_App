4fa1e032377eb10b96726e7604037c9d0dfddaad
nikolamanushev48 <nikolamanushev48@gmail.com>
2021-06-09 20:49:40 +0300

    Commit whole movement process

--- app/src/main/java/com/example/first_project/MainActivity.kt
+++ app/src/main/java/com/example/first_project/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.first_project
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -9,17 +11,31 @@ import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("SetTextI18n")
+    var x_Down:Float = 0.0F
+    var y_Down:Float = 0.0F
+    @SuppressLint("SetTextI18n","ShowToast", "ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.button)
         val queue = Volley.newRequestQueue(this)
         val text = findViewById<TextView>(R.id.text2)
+        val actionBar = supportActionBar
+        actionBar!!.hide()
+        var listener = View.OnTouchListener(function = { view, motionEvent ->
 
+            if (motionEvent.action == MotionEvent.ACTION_MOVE) {
+
+                view.y = motionEvent.rawY - view.height/2
+                view.x = motionEvent.rawX - view.width/2
+            }
+
+            true
+        })
         val button: Button = findViewById(R.id.button2)
         button.setOnClickListener(){
             val url = //address of server
@@ -30,12 +46,26 @@ class MainActivity : AppCompatActivity() {
                     if(response.toInt() == 1) {
                         setContentView(R.layout.activity_main)
 
-                        //move of whites
+                        white_pawn_1.setOnTouchListener(listener)
+                        white_pawn_2.setOnTouchListener(listener)
+                        white_pawn_3.setOnTouchListener(listener)
+                        white_pawn_4.setOnTouchListener(listener)
+                        white_pawn_5.setOnTouchListener(listener)
+                        white_pawn_6.setOnTouchListener(listener)
+                        white_pawn_7.setOnTouchListener(listener)
+                        white_pawn_8.setOnTouchListener(listener)
                     }
 
                     if(response.toInt() == 2) {
                         setContentView(R.layout.activity_main2)
-                        //move of blacks
+                        black_pawn_1.setOnTouchListener(listener)
+                        black_pawn_2.setOnTouchListener(listener)
+                        black_pawn_3.setOnTouchListener(listener)
+                        black_pawn_4.setOnTouchListener(listener)
+                        black_pawn_5.setOnTouchListener(listener)
+                        black_pawn_6.setOnTouchListener(listener)
+                        black_pawn_7.setOnTouchListener(listener)
+                        black_pawn_8.setOnTouchListener(listener)
                     }
                 },
                 { volleyError ->
